e98dfb641960354eb2dc721f4e0371b68a6637c1
Gustavo Demetrius dos Santos <90574639+Gustavo-Demetrius@users.noreply.github.com>
2022-03-16 20:20:33 -0300

    Create ATV2_d11211102302_r11211100722.java
    te amo professor <3

---
+++ src/olamundo/ATV2_d11211102302_r11211100722.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atv2;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class ATV2_d11211102302_r11211100722 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("RGM: 11211102302 // 11211100722");
+         System.out.println("Gustavo Demetrius // Lucas Reis");
+    System.out.println("11211102302 // 11211100722");
+    System.out.println("18 // 21");
+    System.out.println("https://github.com/Gustavo-Demetrius");
+    System.out.println("www.linkedin.com/in/gustavo-demetrius-dos-santos-684471213");
+    System.out.println("SISTEMAS DE INFORMAÇÃO - BEL // PERÍODO 03 TURMA A ");}
+}

bc51cb17fcb5aa07a79d2500e971c2cb729455e8
Vitinho <79619371+1Luckk@users.noreply.github.com>
2022-03-16 20:32:55 -0300

    Create V11211100596.java
    Victor Ferreira Silva: 11211100596
    Pedro Ferreira Martins: 11201100435
    Raphael Ninomiya: 11211100998

---
+++ src/olamundo/V11211100596.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade2;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class V11211100596 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Pedro Ferreira Martins, RGM: 11201100435, Idade: 19, GitHub: Pedro-FMartins, LinkedIn: Pedro Ferreira Martins, Curso: Sistema de Informação, Turma: Noite"+"\n");
+        System.out.print("Victor ferreira silva, RGM:11211100596, Idade:19, GitHub:Victor_silva8663, LinkedIn: Victor Ferreira Silva, Curso: Sistema de Informação, Turma: Noite"+"\n");
+        System.out.print("Raphael Ninomiya, RGM: 11211100998, Idade: 18, GitHub: Raphael-Ninomiya, LinkedIn: Raphael Ninomiya,Curso: Sistema de Informação, Turma: Noite"+"\n");
+    }
+    
+}

84b332984f4b616f1740f070c106e80b910b2ffa
limo442 <100251937+limo442@users.noreply.github.com>
2022-03-16 20:22:08 -0300

    Create A11211102827.java

---
+++ src/olamundo/A11211102827.java
@@ -0,0 +1,21 @@
+
+ 
+package atv2;
+
+
+public class A11211102827 {
+
+    
+    
+    public static void main(String[] args) {
+        
+      System.out.println("11211102827/11211101240");
+      System.out.println("Gabriel Paulosi de Freitas/Rafhael Banuls Lopes");
+      System.out.println("18 anos/18 anos");
+      
+     
+    
+        
+    }
+    
+}

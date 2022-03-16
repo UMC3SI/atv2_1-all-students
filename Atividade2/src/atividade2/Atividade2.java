ef0861f35e7ddb23c30e9f840ce786d3ecf61fcf
Pho3nix999 <Leonardob999usa@gmail.com>
2022-03-16 19:45:00 -0300

    Feat
    Trabalho java atv2

---
+++ Atividade2/src/atividade2/Atividade2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade2;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Atividade2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Leonardo Piascentini Bugmann ");
+        System.out.println("11211100076 ");
+        System.out.println("19 anos ");
+        System.out.println("@Pho3nix999 ");
+        System.out.println("@LeonardoPiascentiniBugmann ");
+        System.out.println("Sistemas de informação turma A ");
+        
+        System.out.println("Lucas Da Silva Cardoso ");
+        System.out.println("11211102438 ");
+        System.out.println("26 anos ");
+        System.out.println("@oeLuk ");
+        System.out.println("@LucasDaSilvaCardoso ");
+        System.out.println("Sistemas de informação turma A ");
+        
+        System.out.println("Renata Cristina Da Silva ");
+        System.out.println("11211100766 ");
+        System.out.println("18 anos ");
+        System.out.println("@ERenata ");
+        System.out.println("@RenataCristina ");
+        System.out.println("Sistemas de informação turma A ");
+        
+    }
+    
+}

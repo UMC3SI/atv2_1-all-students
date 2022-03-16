3b531e53c227e7c642221f4764383a90f50949c5
grupolm3 <umcgrupolm3@gmail.com>
2022-03-16 20:19:15 -0300

    feat(dados)
    Informações do grupo

---
+++ src/olamundo/x11211101115.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atv_2;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class x11211101115 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nome: Lucas Torino, Maria Clara, Matheus Cardoso, Marcos Antonio");
+        System.out.println("RGM: 11211101115, 11211101482, 11211100367, 11211501848");
+        System.out.println("Idade: 20, 19, 20, 24");
+        System.out.println("Turma e Curso: 3A Sistema de Informação");
+    }
+    
+}

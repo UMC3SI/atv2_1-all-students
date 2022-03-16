6a7606568a1a7dc0ddd36634169a5114541d354b
Allan475 <100589208+Allan475@users.noreply.github.com>
2022-03-16 20:23:04 -0300

    Entrega da Ativ_2
    Allan  - 11211102262
    Breno - 11211102037

---
+++ src/allan11211102262/Allan11211102262Breno11211102037.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package allan11211102262;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Allan11211102262Breno11211102037 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("=====================================");
+        System.out.println("Allan de Oliveira Borges Ricardo");
+        System.out.println("11211102262");
+        System.out.println("19");
+        System.out.println("Allan475");
+        System.out.println("Allan de Oliveira");
+        System.out.println("sistema da informação - turma 3 A");
+        //
+        System.out.println("=====================================");
+        System.out.println("Breno Zanuctchel Maldonado Santos");
+        System.out.println("        11211102037");
+        System.out.println("             19");
+        System.out.println("     Github.com/FlameFau");
+        System.out.println("      Breno Zanuctchel");
+        System.out.println("sistema da informação - turma 3 A");
+        System.out.println("=====================================");
+    }
+    
+}

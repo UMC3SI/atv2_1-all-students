be921d7070b795a755bfb4742e8889d164529e53
GABRIEL FERRI <gabriel.ferri@outlook.com>
2022-03-16 20:16:42 -0300

    Create Gabriel11211102207.java

---
+++ src/olamundo/Gabriel11211102207.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gabriel11211102207;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Gabriel11211102207 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+    System.out.println("Gabriel Monteiro Farias "
+            + " 11211102064 "
+            + " 19 anos "
+            + " Sistema de informação "
+            + " Terceiro ");}
+}

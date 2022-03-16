641ce3d154c1b18a26b446f4780a7c4c1e9ad3d0
Gui-4ntun3s <giulhermeg@gmail.com>
2022-03-16 20:20:03 -0300

    Create Antunes11211102142shibakura11211102084nicolas11211100618.java

---
+++ src/olamundo/Antunes11211102142shibakura11211102084nicolas11211100618.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Antunes11211102142shibakura11211102084nicolas11211100618 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nome: Rafael Tavares e Guilherme Antunes"
+                + "RGM: 11182100715, 11211102142"
+                + "Idade: 22 e 18"
+                + "GITHUB: rafaeltavares27 e Gu1-4ntun3s"
+                + "LinkdIn: Rafael Tavares e Guilherme Antunes"
+                + "Sistemas de Informações - 3ºA");
+        // TODO code application logic here
+    }
+    
+}

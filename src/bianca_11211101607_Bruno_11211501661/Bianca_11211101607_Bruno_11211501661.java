7c3db601eff0943f50e93ca5aafe9902139b2244
Bianca Cristina Gomes da Silva <bianca.eb0705@gmail.com>
2022-03-16 20:21:07 -0300

    Entrega da Atividade

---
+++ src/bianca_11211101607_Bruno_11211501661/Bianca_11211101607_Bruno_11211501661.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bianca_11211101607_Bruno_11211501661;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Bianca_11211101607_Bruno_11211501661 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bianca Gomes");
+        System.out.println("11211101607");
+        System.out.println("18 anos");
+        System.out.println("biagomess");
+        System.out.println("biancacristinagomes");
+        System.out.println("Sistemas de Informção - 3º A");
+        System.out.println();
+        System.out.println("Bruno Moreira");
+        System.out.println("11211501661");
+        System.out.println("22 anos");
+        System.out.println("brunordgsmrr");
+        System.out.println("bruno-rodrigues-moreira-7609a5191");
+        System.out.println("Sistemas de Informção - 3º A");
+    }
+    
+}

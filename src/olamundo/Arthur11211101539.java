8d349ef9af46291c14d6abbd12dc9840cb50242e
ARTHUR ALBINO PIRES <100172812+Parthur15@users.noreply.github.com>
2022-03-16 20:22:14 -0300

    Create Arthur11211101539.java

---
+++ src/olamundo/Arthur11211101539.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Arthur11211101539 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println(" Arthur Albino Pires_Caio Santos de Vasconcelos"
+                         + " 11211101539_11211101570 "
+                         + "19 Anos_22 anos -"
+                         + " Sistemama Da Informação "
+                         + " Terceiro Semestre");
+        }
+    
+}

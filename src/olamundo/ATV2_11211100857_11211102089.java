c88d8bdc87e1809c563c2ee80de5dc2cc5bc0b2b
Leonardo Ferreira <56656888+leonardo-ferreiraa@users.noreply.github.com>
2022-03-16 20:20:14 -0300

    Create ATV2_11211100857_11211102089.java

---
+++ src/olamundo/ATV2_11211100857_11211102089.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atv2;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class ATV2_11211100857_11211102089 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+  
+        System.out.println("Nome: Leonardo Ferreira da Silva Vieira // Matheus Meirielli Miranda Freitas da Silva");
+        System.out.println("RGM: 11211100857 // 11211102089");
+        System.out.println("Idade: 20 anos // 18 anos");
+        System.out.println("GitHub: https://github.com/leonardo-ferreiraa // https://github.com/AdrianVeidt24?classId=fb3b16f7-298e-4c46-9106-899448898b02&assignmentId=cfd65e7f-94b6-4624-a3bf-f4e852f422cc&submissionId=5fbd8c3e-321f-4e42-dd5a-3a0b8c0e37bf");
+        System.out.println("LinkedIn: https://www.linkedin.com/in/leonardo-ferreira-da-silva-vieira-58190421b/?classId=fb3b16f7-298e-4c46-9106-899448898b02&assignmentId=cfd65e7f-94b6-4624-a3bf-f4e852f422cc&submissionId=409daee9-2a12-9f33-3438-1ed5951d0aa6 // https://www.linkedin.com/in/matheus-meirielli-a69b64229/?classId=fb3b16f7-298e-4c46-9106-899448898b02&assignmentId=cfd65e7f-94b6-4624-a3bf-f4e852f422cc&submissionId=5fbd8c3e-321f-4e42-dd5a-3a0b8c0e37bf");
+        System.out.println("Curso: Sistema de Informação // Turma: 3A");
+        // TODO code application logic here
+    }
+    
+}

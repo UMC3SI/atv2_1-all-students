65b9f6ee11f31e98c385a0d62553fcdce391b329
FidelisMM <74383616+FidelisMM@users.noreply.github.com>
2022-03-16 21:27:43 -0300

    feat(meus-dados)
    Adicionando meus dados na main.

---
+++ src/olamundo/RGM_11211100176.java
@@ -0,0 +1,15 @@
+
+package atividade2;
+
+public class RGM_11211100176 {
+    
+public static void main(String[] args) {
+        System.out.println("Nome Completo: Matheus Medeiros Fidelis");
+        System.out.println("RGM: 11211100176");
+        System.out.println("Idade: 18");
+        System.out.println("Seu @gitHub: @FidelisMM");
+        System.out.println("Seu @LinkedIn: .../fidelis-matheus");
+        System.out.println("Curso e Turma: Sistemas de Informação - 3°Sem. Noturno");
+    }    
+    
+}
